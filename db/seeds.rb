2473a858b2b65d8e2475cb136ae997946cd2fbb7
Zachary Ehren <zachehren@gmail.com>
2017-03-18 18:37:19 -0700

    checkpoint 17 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+#Create Posts
+50.times do
+#we use create! with a bang(!). Adding a ! instructs the method to raise
+#an error if there's a probelem with the data we're seeding. Using create without a bang
+#could fail without warning, causing the error to surface later
+  Post.create!(
+  #we use methods from a class that doesn't exist yet (RandomData) that will create
+  #random strings for title and body. Writing code for classes and methods that don't exist
+  #yet is known as "wishful coding" and can increase productivity because it allows you to stay focused on one problem at a time
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+posts = Post.all
+
+#Create Comments
+#we call times on an Integer. This will run a given block the specified # of times
+100.times do
+  Comment.create!(
+  #we call sample on the array returned by Post.all in order to pick a random post to associate each comment with
+  #sample returns a random element from the array every time it's called
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
---
+++ lib/random_data.rb
@@ -0,0 +1,37 @@
+#we define RandomData as a module because it is a standalone library with no dependencies or inheritance requirements
+module RandomData
+#we define random_paragraph
+  def self.random_paragraph
+  #we set sentences to an array
+    sentences = []
+    #we create 4 - 6 random sentences and append them to sentences
+    rand(4..6).times do
+      sentences << random_sentence
+    end
+    #we call join on sentences to combine each sentence in the array, passing a space to separate each sentence
+    #join combines each sentence into a full paragraph (as a string)
+    sentences.join(" ")
+  end
+
+#we follow the same pattern as we did above to create a sentence with a random number of words
+  def self.random_sentence
+    strings = []
+    rand(3..8).times do
+      strings << random_word
+    end
+
+    sentence = strings.join(" ")
+    #after we generate a sentence, we call capitalize on it and append a period
+    #capitalize returns a copy of sentence with the first character converted to uppercase and the remainder converted to lowercase
+    sentence.capitalize << "."
+  end
+
+  def self.random_word
+    #we set letters to an array of letters a through z using to_a
+    letters = ('a'..'z').to_a
+    #we call shuffle! on letters in place. If we were to call shuffle without the bang, shuffle would return rather than shuffle in place
+    letters.shuffle!
+    #we join the zeroth through nth item in letters. The nth item is the result of rand(3..8) which returns a random number greater than or equal to three and less than 8
+    letters[0,rand(3..8)].join
+  end
+end
