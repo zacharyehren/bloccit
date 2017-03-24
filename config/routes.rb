84c950fb1b321db54a977d668794919ace64a2d5
Zachary Ehren <zachehren@gmail.com>
2017-03-23 20:16:28 -0700

    assignment 20 almost complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+
   #we call the resources method and pass it a symbol. this instructs Rails to create
   #post routes for creating viewing & deleting instances of Post
   resources :posts
+  resources :question 
 
   #We removed `get "welcome/index"` because we've declared the index view as the root view
   #we also modify the about route (get "welcome/about) to allow users to visit /about rather than /welcome/about
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) {Question.create!(title: "New Question Title", body:"New Question Body", resolved: false)}
-
   describe "attributes" do
-    it "has title, body and resolved attributes" do
-      expect(question).to have_attributes(title: "New Question Title", body:"New Question Body", resolved: false)
+    let(:question) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
+
+    it "should repond to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "shoudl respond to resolved" do
+      expect(question).to respond_to(:resolved)
     end
   end
 end
