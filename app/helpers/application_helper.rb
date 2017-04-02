614849104825172fe83cf85c988ae2dde25c866b
Zachary Ehren <zachehren@gmail.com>
2017-04-02 13:04:52 -0700

    assignment 26 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   #This content_tag helper method is used to build the HTML & css to display the form element & any associated errors
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+   
 end
